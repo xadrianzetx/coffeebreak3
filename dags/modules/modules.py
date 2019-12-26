db38aa6edf5f94ea6012ba3ae2ed3399a2cd8fbc
xadrianzetx <xadrianzetx@gmail.com>
2019-12-26 18:06:16 +0000

    test components of pipeline

--- dags/modules/modules.py
+++ dags/modules/modules.py
@@ -1,8 +1,25 @@
+import requests
 from datetime import datetime
 
 
 def check_scheduler():
+    """
+    Test Airflow scheduler heartbeat
+    """
     exec_time = datetime.now()
     msg = 'ran at {}'.format(exec_time)
-
     return msg
+
+def check_network():
+    """
+    Test network connection within Docker container
+    """
+    # ping google
+    r = requests.get('http://216.58.192.142')
+    
+    if r.status_code == 200:
+        # network reachable inside container
+        return 'connection ok'
+    
+    else:
+        return 'no connection'
--- dags/timer.py
+++ dags/timer.py
@@ -7,16 +7,30 @@ from modules import modules
 setup = {
     'owner': 'xadrianzetx',
     'depends_on_past': False,
-    'start_date': datetime.now() - timedelta(minutes=1),
+    'start_date': datetime.now() - timedelta(minutes=2),
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('scheduler_test', default_args=setup, schedule_interval=timedelta(minutes=1))
+dag = DAG(
+    'pipeline_test',
+    default_args=setup,
+    schedule_interval=timedelta(minutes=1)
+)
 
 t1 = PythonOperator(
-    task_id='test',
+    task_id='scheduler',
     provide_context=False,
     python_callable=modules.check_scheduler,
     dag=dag
 )
+
+t2 = PythonOperator(
+    task_id='network',
+    provide_context=False,
+    python_callable=modules.check_network,
+    dag=dag
+)
+
+# set order of execution
+t2.set_upstream(t1)
